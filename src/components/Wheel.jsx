575d32764a732490735de9620dc5f70471276736
Joseph Ciaravella <joseph.ciaravella@mail.mcgill.ca>
2025-01-23 23:48:29 -0500

    initial commit?

---
+++ src/components/Wheel.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../components/Wheel.css"
+
+export default function Wheel() {
+    return(
+        <div>
+            {/* <svg className="wheel-container">
+                <rect className="wheel-rect"/>
+            </svg> */}
+            
+        </div>
+    )
+}
